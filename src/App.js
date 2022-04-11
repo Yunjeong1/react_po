cdb3d9c570a8a3ab0f96a3cf21d2d5a748c4eed2
Yunjeong <8768moon@naver.com>
2022-04-11 14:53:48 +0900

    route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route } from 'react-router-dom';
+import './scss/style.scss';
+//common 컴포넌트
+import Header from './components/common/Header';
+import Footer from './components/common/Footer';
+//main 컴포넌트
+import Visual from './components/main/Visual';
+import Content from './components/main/Content';
+//sub 컴포넌트
+import Youtube from './components/sub/Youtube';
+import Gallery from './components/sub/Gallery';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Header />
+
+			<Route exact path='/'>
+				<Visual />
+				<Content />
+			</Route>
+
+			<Route path='/youtube' component={Youtube} />
+			<Route path='/gallery' component={Gallery} />
+
+			<Footer />
+		</>
+	);
 }
 
 export default App;
