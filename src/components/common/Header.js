a1012296e9f45c445523290e80961f936654100f
Yunjeong <8768moon@naver.com>
2022-04-11 15:34:39 +0900

    NavLink

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
+	const active = { color: 'aqua' };
 	return (
 		<header>
-			<h1>Header</h1>
+			<div className='inner'>
+				<h1>
+					<NavLink exact='true' to='/' activeStyle={active}>
+						LOGO
+					</NavLink>
+				</h1>
+
+				<ul id='gnb'>
+					<li>
+						<NavLink to='/gallery' activeStyle={active}>
+							Gallery
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/youtube' activeStyle={active}>
+							Youtube
+						</NavLink>
+					</li>
+				</ul>
+			</div>
 		</header>
 	);
 }
