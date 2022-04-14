1a92d89a3a425c0ab27070fd5afe23b02bebd65f
Yunjeong <8768moon@naver.com>
2022-04-14 09:30:31 +0900

    marker img

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -2,6 +2,7 @@ import { useRef, useEffect, useState } from 'react';
 import Layout from '../common/Layout';
 
 function Location() {
+	const path = process.env.PUBLIC_URL;
 	//가상돔을 참조할 container객체를 useRef로 생성
 	const container = useRef(null);
 	//window 전역 객체안에서 kakao라는 객체를 찾은 후, kakao라는 변수이름으로 비구조화 할당
@@ -14,14 +15,35 @@ function Location() {
 
 	useEffect(() => {
 		const options = {
-			center: new kakao.maps.LatLng(33.450701, 126.570667),
+			center: new kakao.maps.LatLng(37.512742, 127.06081),
 			level: 3,
 		};
 
 		//첫번째 인수로 지도가 들어갈 프레임 등록, 두번째 인수로 지도위치, 줌레벨 옵션값 등록해서 인스턴스 생성
-		const map = new kakao.maps.Map(container.current, options);
+		const mapInfo = new kakao.maps.Map(container.current, options);
 		//인스턴스 map을 state map으로 옮겨담음
-		setMap(map);
+		setMap(mapInfo);
+
+		//마커 위치 인스턴스 생성
+		const markerPosition = new kakao.maps.LatLng(37.512742, 127.06081);
+
+		//마커 이미지 추가
+		const imgSrc = `${path}/img/marker1.png`;
+		const imgSize = new kakao.maps.Size(232, 99);
+		const imgOption = { offset: new kakao.maps.Point(110, 90) };
+		const markerImg = new kakao.maps.MarkerImage(imgSrc, imgSize, imgOption);
+
+		// 마커를 생성합니다
+		const marker = new kakao.maps.Marker({
+			position: markerPosition,
+			image: markerImg,
+		});
+
+		// 마커가 지도 위에 표시되도록 설정합니다
+		marker.setMap(mapInfo);
+
+		// 아래 코드는 지도 위의 마커를 제거하는 코드입니다
+		// marker.setMap(null);
 	}, []);
 
 	//traffic값이 변경될때마다 실행되는 useEffect 호출
